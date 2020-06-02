cc5f5030b20895159e3601ccda0608a171174f60
cam-per <minkotaras@gmail.com>
2020-06-02 20:45:01 +0300

    fix export

--- build_windows.sh
+++ build_windows.sh
@@ -3,8 +3,8 @@
 rm -rf ./build
 mkdir ./build
 
-CC=x86_64-w64-mingw32-gcc 
-CGO_ENABLED=1 
-GOOS=windows 
-GOARCH=amd64
+export CC=x86_64-w64-mingw32-gcc 
+export CGO_ENABLED=1 
+export GOOS=windows 
+export GOARCH=amd64
 go build -v -o ./build/client.exe github.com/yeahyeahcore/HardwareMonitorNET/cmd/client 
